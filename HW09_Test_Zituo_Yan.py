caaa23e2ee2e5013a8d3a1a91c33f3296da0fc3e
capWailing <thomasyantom@126.com>
2019-11-06 17:59:18 -0500

    achieve basic functionality
    complete basic functionality

--- HW09_Test_Zituo_Yan.py
+++ HW09_Test_Zituo_Yan.py
@@ -2,19 +2,6 @@ import unittest
 from HW09_Zituo_Yan import *
 
 
-class TestFileReadingGen(unittest.TestCase):
-    def test_file_reading_gen(self):
-        self.assertEqual(list(file_reading_gen('../student_majors.txt', 3, sep='|', header=True)),
-                         [('123', 'Jin He', 'Computer Science'), ('234', 'Nanda Koka', 'Software Engineering'),
-                          ('345', 'Benji Cai', 'Software Engineering')])
-        self.assertEqual(list(file_reading_gen('../student_majors_without_header.txt', 3, sep='|', header=False)),
-                         [('123', 'Jin He', 'Computer Science'), ('234', 'Nanda Koka', 'Software Engineering'),
-                          ('345', 'Benji Cai', 'Software Engineering')])
-        with self.assertRaises(FileNotFoundError):
-            for a in file_reading_gen('../123.txt', 3, ' ', True):
-                print(a)
-
-
 class TestRepository(unittest.TestCase):
     def test_repository(self):
         # test file with no grade in grades.txt
@@ -24,13 +11,13 @@ class TestRepository(unittest.TestCase):
             Repository('./test2')
 
         stevens = Repository('./stevens')
-        self.assertEqual(stevens.student['10103'].cwid, '10103')
-        self.assertEqual(stevens.student['10103'].name, 'Baldwin, C')
-        self.assertEqual(stevens.student['10103'].get_summary(), ['10103', 'Baldwin, C', ['SSW 567', 'SSW 564', 'SSW 687', 'CS 501']])
-        self.assertEqual(stevens.instructor['98765'].cwid, '98765')
-        self.assertEqual(stevens.instructor['98765'].name, 'Einstein, A')
-        self.assertEqual(stevens.instructor['98765'].get_summary(), [['98765', 'Einstein, A', 'SFEN', 'SSW 567', 4],
-                         ['98765', 'Einstein, A', 'SFEN', 'SSW 540', 3]])
+        self.assertEqual(stevens._student['10103'].cwid, '10103')
+        self.assertEqual(stevens._student['10103'].name, 'Baldwin, C')
+        self.assertEqual(stevens._student['10103'].get_summary(), ['10103', 'Baldwin, C', ['SSW 567', 'SSW 564', 'SSW 687', 'CS 501']])
+        self.assertEqual(stevens._instructor['98765'].cwid, '98765')
+        self.assertEqual(stevens._instructor['98765'].name, 'Einstein, A')
+        self.assertEqual(stevens._instructor['98765'].get_summary(), [['98765', 'Einstein, A', 'SFEN', 'SSW 567', 4],
+                                                                      ['98765', 'Einstein, A', 'SFEN', 'SSW 540', 3]])
 
 
 if __name__ == '__main__':
--- HW09_Zituo_Yan.py
+++ HW09_Zituo_Yan.py
@@ -8,6 +8,16 @@ from collections import defaultdict
 from prettytable import PrettyTable
 
 
+def list_difference(l1, l2):
+    """
+        return difference of two lists
+    :param l1:
+    :param l2:
+    :return:
+    """
+    return [i for i in l1 if i not in l2]
+
+
 def file_reading_gen(path, fields, sep=',', header=False):
     """
         field separated file reader
@@ -20,7 +30,7 @@ def file_reading_gen(path, fields, sep=',', header=False):
     try:
         fp = open(path)
     except FileNotFoundError:
-        raise FileNotFoundError(f"File {path} is not found")
+        print(f"File {path} is not found")
     else:
         with fp:
             if header:
@@ -28,7 +38,7 @@ def file_reading_gen(path, fields, sep=',', header=False):
             for offset, line in enumerate(fp):
                 lines = line.strip().split(sep)
                 if len(lines) != fields:
-                    raise ValueError(f'{path} has {len(lines)} fields on line {offset + 1} but expected {fields}')
+                    raise ValueError(f"{path} has wrong fields. Should be {fields} fields, separate by '{sep}'.")
                 else:
                     yield tuple(lines)
 
@@ -39,15 +49,19 @@ class Student:
         methods: add_course_grade
                 get_summary
     """
+
     def __init__(self, *args):
         """
             initiate student class
         :param args:
         """
-        self.cwid = args[0]
-        self.name = args[1]
+        self._cwid = args[0]
+        self._name = args[1]
         self.major = args[2]
         self._course_grade = defaultdict(str)
+        self._required = list()
+        self._electives = list()
+        self._completed_courses = list()
 
     def add_course_grade(self, course, grade):
         """
@@ -57,13 +71,19 @@ class Student:
         :return:
         """
         self._course_grade[course] = grade
+        if grade != "F":
+            self._completed_courses.append(course)
+
+    def add_remaining(self, major):
+        self._required = list_difference(major.required, self._completed_courses)
+        self._electives = None if set(major.electives) & set(self._completed_courses) else major.electives
 
     def get_summary(self):
         """
             return summary for pretty table
         :return:
         """
-        return [self.cwid, self.name, [key for key in self._course_grade.keys()]]
+        return [self._cwid, self._name, self._completed_courses, self._required, self._electives]
 
 
 class Instructor:
@@ -72,6 +92,7 @@ class Instructor:
         methods:    add_student_to_course
                     get_summary
     """
+
     def __init__(self, *args):
         """
             initiate instructor class
@@ -98,80 +119,161 @@ class Instructor:
         return [[self.cwid, self.name, self.department, key, value] for key, value in self._course_student.items()]
 
 
+class Major:
+    """
+        Major class
+        methods:    add_required
+                    add_electives
+                    get_summary
+    """
+
+    def __init__(self, major):
+        """
+            initiate major class
+        :param major:
+        """
+        self._major = major
+        self.required = list()
+        self.electives = list()
+
+    def add_courses(self, course, attitude):
+        """
+            add required course for single major
+        :param attitude:
+        :param course:
+        :return:
+        """
+        self.required.append(course) if attitude == "R" else self.electives.append(course)
+
+    def get_summary(self):
+        """
+            return summary for prettytable
+        :return:
+        """
+        return [self._major, self.required, self.electives]
+
+
 class Repository:
     """
-        repository class for collect data and print from students.txt instructors.txt and grades.txt
-        methods:    storage_student
-                    storage_instructor
-                    storage_grade
-                    summary
-                    repository
+            repository class for collect data and print from students.txt instructors.txt and grades.txt
+            methods:    storage_student
+                        storage_instructor
+                        storage_grade
+                        summary
+                        repository
     """
-    def __init__(self, dir_path):
+
+    student_table = PrettyTable()
+    student_table.field_names = ['CWID', 'Name', 'Completed Courses', 'Remaining Required', 'Remaining Elective']
+    instructor_table = PrettyTable()
+    instructor_table.field_names = ['CWID', 'Name', 'Dept', 'Courses', 'Students']
+    major_table = PrettyTable()
+    major_table.field_names = ['Major', 'Required', 'Electives']
+
+    def __init__(self, dir_path, default_print=True):
         """
             initiate repository
         :param dir_path:
         """
-        self.student_directory = os.path.join(dir_path, "students.txt")
-        self.instructor_directory = os.path.join(dir_path, "instructors.txt")
-        self.grade_directory = os.path.join(dir_path, "grades.txt")
-        self.student = dict()
-        self.instructor = dict()
-        self.repository()
+        self._student_directory = os.path.join(dir_path, "students.txt")
+        self._instructor_directory = os.path.join(dir_path, "instructors.txt")
+        self._grade_directory = os.path.join(dir_path, "grades.txt")
+        self._major_directory = os.path.join(dir_path, "majors.txt")
+        self._student = dict()
+        self._instructor = dict()
+        self._major = dict()
+        self.repository(default_print)
 
     def storage_student(self):
         """
             read and save data from students.txt
         :return:
         """
-        for students in file_reading_gen(self.student_directory, 3, '\t'):
-            self.student[students[0]] = Student(*students)
+        for students in file_reading_gen(self._student_directory, 3, ';', True):
+            for values in students:
+                if len(values) == 0:
+                    raise ValueError("Students table has empty value!")
+            self._student[students[0]] = Student(*students)
 
     def storage_instructor(self):
         """
             read and save data from instructors.txt
         :return:
         """
-        for instructors in file_reading_gen(self.instructor_directory, 3, '\t'):
-            self.instructor[instructors[0]] = Instructor(*instructors)
+        for instructors in file_reading_gen(self._instructor_directory, 3, '|', True):
+            for values in instructors:
+                if len(values) == 0:
+                    raise ValueError("Instructors table has empty value!")
+                else:
+                    self._instructor[instructors[0]] = Instructor(*instructors)
 
     def storage_grade(self):
         """
             read and process grade
         :return:
         """
-        for records in file_reading_gen(self.grade_directory, 4, '\t'):
-            student = self.student[records[0]]
-            student.add_course_grade(records[1], records[2])
-            instructor = self.instructor[records[3]]
+        for records in file_reading_gen(self._grade_directory, 4, '|', True):
+            try:
+                student = self._student[records[0]]
+            except KeyError:
+                raise KeyError(f"{records[0]} are not in student table!")
+            if records[2] not in ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'F']:
+                raise ValueError("Grades invalid, should be 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C' and 'F'.")
+            else:
+                student.add_course_grade(records[1], records[2])
+                student.add_remaining(self._major[student.major])
+            try:
+                instructor = self._instructor[records[3]]
+            except KeyError:
+                raise KeyError(f"{records[3]} are not in instructors table!")
             instructor.add_student_to_course(records[1])
+
+    def storage_major(self):
+        """
+            read and process major
+        :return:
+        """
+        for major in file_reading_gen(self._major_directory, 3, '\t', True):
+            if not self._major.get(major[0]):
+                self._major[major[0]] = Major(major[0])
+            if major[1] != "R" and major[1] != "E":
+                raise ValueError(f"Major's required or elective field should be R or E!")
+            else:
+                self._major[major[0]].add_courses(major[2], major[1])
 
     def summary(self):
         """
             form and print prettytable
         :return:
         """
-        student_table = PrettyTable()
-        student_table.field_names = ['CWID', 'Name', 'Completed Courses']
-        instructor_table = PrettyTable()
-        instructor_table.field_names = ['CWID', 'Name', 'Dept', 'Courses', 'Students']
-        for students in self.student.values():
-            student_table.add_row(students.get_summary())
-        for instructors in self.instructor.values():
+        for students in self._student.values():
+            self.student_table.add_row(students.get_summary())
+        for instructors in self._instructor.values():
             for record in instructors.get_summary():
-                instructor_table.add_row(record)
-        print(student_table)
-        print(instructor_table)
+                self.instructor_table.add_row(record)
+        for major in self._major.values():
+            self.major_table.add_row(major.get_summary())
+        print(self.student_table)
+        print(self.instructor_table)
+        print(self.major_table)
 
-    def repository(self):
+    def repository(self, default_print):
         """
             start process the class
         :return:
         """
-        self.storage_student()
-        self.storage_instructor()
-        self.storage_grade()
-        self.summary()
+        try:
+            self.storage_major()
+            self.storage_student()
+            self.storage_instructor()
+            self.storage_grade()
+        except KeyError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
+        else:
+            if default_print:
+                self.summary()
 
 
 def main():
